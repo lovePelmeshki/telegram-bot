52aee148c976b14e5a3397920aa4d4589b87764e
lovePelmeshki <love.pelmeshki@gmail.com>
2022-03-18 16:22:16 +0300

    add mysql

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "user_user",
+    database: "telegram-bot-db",
+    password: "90f8b7rR@"
+});
+connection.connect(function(err){
+    if (err){
+        return console.error("Error: " + err)
+    }
+    else{
+        console.log("Connection SUSSED!")
+    }
+})
+
+
 
 const TelegramApi = require('node-telegram-bot-api')
 const token = '5168847385:AAGvEGSIWXLnVXFCwp83atbUYQKDb7E1yfg'
@@ -7,16 +24,15 @@ const bot = new TelegramApi(token, {polling: true})
 
 bot.setMyCommands([
     {command: '/start', description: 'Начать'},
-    {command: '/new_event', description: '+Новая заявка'},
-    {command: '/show_events', description: 'Показать заявки'}
+    {command: '/new_event', description: 'Новая заявка'},
+    {command: '/show_events', description: 'Показать заявки'},
+    {command: '/whoami', description: 'Показать имя и фамилию'}
 ])
 
 
 bot.on('message', async msg =>{
     const text = msg.text;
     const chatId = msg.chat.id;
-
-
     if (text === '/start'){
         console.log(msg);
         await bot.sendMessage(chatId, `chatId = ${chatId} \nid = ${msg.from.id} \nusername = ${msg.from.username}`)
@@ -25,8 +41,10 @@ bot.on('message', async msg =>{
         await bot.sendMessage(chatId, `bot.sendMessage ${text}`)
     }
     if (text === '/show_events'){
-        await  bot.sendMessage(chatId, `bot.sendMessage ${text}`)
-    }
+        await  bot.sendMessage(chatId, `bot.sendMessage ${text}`)}
+    if (text === '/whoami'){
+        await bot.sendMessage(chatId, `first_name = ${msg.from.first_name}, name = ${msg.from.name}, last_name = ${msg.from.last_name}` )
+    } else await  bot.sendMessage(chatId, text)
 })
 
 bot.on('callback_query', msg =>{
@@ -34,3 +52,5 @@ bot.on('callback_query', msg =>{
     const chatId = msg.message.chat.id;
     console.log(msg)
 })
+
+//changes
